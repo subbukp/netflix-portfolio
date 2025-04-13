c2dbfda188cdef8adf6b528d62eac03ceb4365b2
Subrahmanya <subrahmanya.kp@qure.ai>
2025-04-13 13:01:38 +0530

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  swcMinify: true,
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    
+    // Speed up development builds
+    if (process.env.NODE_ENV === 'development') {
+      config.watchOptions = {
+        ignored: ['**/node_modules', '**/.git'],
+        poll: 1000, // Check for changes every second
+      }
+    }
+    
+    return config;
+  },
 }
 
 module.exports = nextConfig 
